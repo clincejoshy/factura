2985b6a704fb614d85d5ebe89a57e893a1822bc3
CLINCE JOSHY <clincepro@gmail.com>
2018-07-05 14:43:28 +0530

    changes for factura 2018

--- zyro/4.php
+++ zyro/4.php
@@ -8,7 +8,7 @@
 		<meta name="description" content="" />
 	<meta name="keywords" content="" />
 		<meta name="generator" content="Zyro - Website Builder" />
-	
+
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
 	<script src="js/bootstrap.min.js" type="text/javascript"></script>
@@ -18,7 +18,7 @@
 	<link href="css/common.css?ts=1472543601" rel="stylesheet" type="text/css" />
 	<link href="css/4.css?ts=1472543601" rel="stylesheet" type="text/css" />
 	<link rel="shortcut icon" href="/gallery/icon.ico" type="image/x-icon" />
-	<script type="text/javascript">var currLang = '';</script>		
+	<script type="text/javascript">var currLang = '';</script>
 	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -27,10 +27,10 @@
 
 
 <body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_header">
-	
+
 </div>
 <div class="vbox wb_container" id="wb_main">
-	
+
 <div id="wb_element_instance31" class="wb_element"><ul class="hmenu"><li><a href="Home/" target="_self" title="Home">Home</a></li><li><a href="About/" target="_self" title="About">About</a></li><li class="active"><a href="Call-For-Papers/" target="_self" title="Call For Papers">Call For Papers</a></li><li><a href="Registration/" target="_self" title="Registration">Registration</a><ul><li><a href="Phase-I-Registration/" target="_self" title="Phase I Registration">Phase I Registration</a></li><li><a href="Phase-II-Paper-Submission/" target="_self" title="Phase II-Paper Submission">Phase II-Paper Submission</a></li><li><a href="Phase-III-Final-Registration/" target="_self" title="Phase III-Final Registration">Phase III-Final Registration</a></li></ul></li><li><a href="Downloads/" target="_self" title="Downloads">Downloads</a></li><li><a href="Committe/" target="_self" title="Committe">Committe</a><ul><li><a href="Organizing/" target="_self" title="Organizing">Organizing</a></li><li><a href="Student-Committe/" target="_self" title="Student Committe">Student Committe</a></li></ul></li><li><a href="Contact-Us/" target="_self" title="Contact Us">Contact Us</a></li></ul></div><div id="wb_element_instance32" class="wb_element" style=" overflow: hidden;"><p style="font-family: Geneva, Arial, Helvetica, sans-serif; font-size: 14pt; line-height: normal; font-weight: bold; font-variant: normal; color: #030339; text-decoration: blink;"><b>⚹<b></b>Conventional and Modern Machining Process</b></p>
 <p style="font-family: Geneva, Arial, Helvetica, sans-serif; font-size: 14pt; line-height: normal; font-weight: bold; font-variant: normal; color: #030339; text-decoration: blink;"><b>⚹<b></b>Machine Vision</b></p>
 <p style="font-family: Geneva, Arial, Helvetica, sans-serif; font-size: 14pt; line-height: normal; font-weight: bold; font-variant: normal; color: #030339; text-decoration: blink;"><b>⚹<b></b>Design for Manufacturing</b></p>
@@ -57,7 +57,7 @@ h1 {
 <style>
 table {
     font-family: arial, sans-serif;
-    
+
     border-collapse: collapse;
     width: 100%;
 }
@@ -74,15 +74,15 @@ tr:nth-child(even) {
 </style><font color="white"><table><tr><th><font color="black"><font color=white>IMPORTANT DATES</font></font></th>
     <th> </th>
   </tr><tr><td><font color="black">Submission of Abstract</font></td>
-    <td><font color="black">May 1 to May 6,2017</font></td>
+    <td><font color="black">13 June 2018</font></td>
   </tr><tr><td>Submission of Paper</td>
-    <td>May 8 to May 13, 2017</td>
+    <td>16 June 2018</td>
 
   </tr><tr><td><font color="black">Acceptance of Paper</font></td>
-    <td><font color="black">May 20 to May 24,2017</font></td>
+    <td><font color="black">18 June 2018</font></td>
 
   </tr><tr><td>Conference Dates</td>
-    <td>June 9th & 10th</td>
+    <td>June 22 & 23 2018</td>
 
   </tr></table></table></div><div id="wb_element_instance35" class="wb_element" style=" overflow: hidden;">
 
@@ -90,7 +90,7 @@ tr:nth-child(even) {
 <style>
 table {
     font-family: arial, sans-serif;
-    
+
     border-collapse: collapse;
     width: 100%;
 }
@@ -123,7 +123,7 @@ tr:nth-child(even) {
 <style>
 table {
     font-family: arial, sans-serif;
-    
+
     border-collapse: collapse;
     width: 100%;
 }
@@ -138,10 +138,10 @@ tr:nth-child(even) {
     background-color: #dddddd;
 }
 </style><table><tr><th>Registration fee shall be send by DD in favour of "FACTURA" payable at Palakkad. </th>
-    
+
   </tr><tr><th>Registration fees includes Conference kit, Lunch and Tea.</th>
 </tr><tr><th>
-After the acceptance of the Abstract soft copy of the Paper in the given format , original paper should be send to factura2k16@gmail.com and the DD taken in favour of "FACTURA" payable at Palakkad should be sent to the correspondence address with duly filled application form via speed post or courier.
+After the acceptance of the Abstract soft copy of the Paper in the given format , original paper should be send to factura2k18@gmail.com and the DD taken in favour of "FACTURA" payable at Palakkad should be sent to the correspondence address with duly filled application form via speed post or courier.
 </th>
 </tr><tr>
 </tr><tr><th>
@@ -181,7 +181,7 @@ Also pay via FACTURA Acc No.: 6345789408Indian Bank, Branch: Akathethara, Palakk
 			?>
 			</div></div>
 <div class="vbox wb_container" id="wb_footer" style="height: 80px;">
-	
+
 <div id="wb_element_instance38" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
 			$(function() {
 				var footer = $(".wb_footer");
